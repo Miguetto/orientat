d29e09978663b94b327d52dca2c0da36c9f31c7b
Miguel Angel Caro <miguelangel.caro@iesdonana.org>
2021-06-09 21:27:27 +0200

    cambios

--- views/recursos/view.php
+++ views/recursos/view.php
@@ -31,7 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <h2 class="section-heading"><?= Html::encode($model->titulo) ?></h2>
                 <blockquote class="blockquote"><?= Html::encode($model->descripcion) ?></blockquote>
                 <p class="contenidoRecurso"><?= Html::encode($model->contenido) ?></p>
-                <p class="contenidoRecurso">
+                <p class="p-0 text-center">
                     <?= Html::a('Ir al enlace', Url::to($model->enlace), ['class' => 'btn btn-info']) ?>
                 </p>
                 <div class="p-0 text-center">
