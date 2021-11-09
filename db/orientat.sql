06da9eb1c364a2324d51356ea1a28d85c3fbce64
Miguel Angel Caro <miguelangel.caro@iesdonana.org>
2021-11-09 16:20:35 +0100

    sigo probando

--- db/orientat.sql
+++ db/orientat.sql
@@ -74,7 +74,7 @@ CREATE TABLE recursos
     , created_at         TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
     , usuario_id         BIGINT       NOT NULL REFERENCES usuarios(id) ON DELETE CASCADE
     , categoria_id       BIGINT       NOT NULL REFERENCES categorias(id) ON DELETE CASCADE
-    , comentario_id      BIGINT                REFERENCES comentarios(id) ON DELETE CASCADE
+    /*, comentario_id      BIGINT                REFERENCES comentarios(id) ON DELETE CASCADE*/
     , imagen             TEXT
     , pdf_pdf            TEXT
     , revisado           BOOLEAN      NOT NULL DEFAULT false
